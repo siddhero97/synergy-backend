8270a27de4a9300b60194d1bcf470ed165e37b96
Sidd <siddhero97@gmail.com>
2024-05-11 15:04:03 -0700

    Added cors

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 // Import required modules
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoute = require('./user/user.route');
 
+const cors = require('cors');
+
 // Create Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
-const CONNECTION_STRING = process.env.mongodb_connection_string;
+const CONNECTION_STRING = process.env.MONGO_URL;
 
+app.use(cors());
 // Express middleware to parse JSON bodies
 app.use(express.json());
 // Can I have a user route?
 app.use('/users', userRoute);
 
 
+
+
+
 // Start the server
 app.listen(PORT, async () => {
     console.log(`Server started on http://localhost:${PORT}`);
     await connectToMongoDB();
- 
+
 });
 
 async function connectToMongoDB() {
     try {
-        await mongoose.connect(mongodb_connection_string, {
+        await mongoose.connect(CONNECTION_STRING, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
