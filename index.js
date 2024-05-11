007938ba3bfc8db43fa5c86a8b9710077be50a1c
udayrajmandke <urmandke@gmail.com>
2024-05-10 17:29:59 -0700

    feat: Convert connection string to env variable.

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ const userRoute = require('./user/user.route');
 // Create Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const CONNECTION_STRING = process.env.mongodb_connection_string;
 
 // Express middleware to parse JSON bodies
 app.use(express.json());
 // Can I have a user route?
 app.use('/users', userRoute);
 
+
 // Start the server
 app.listen(PORT, async () => {
     console.log(`Server started on http://localhost:${PORT}`);
     await connectToMongoDB();
-
+ 
 });
 
 async function connectToMongoDB() {
     try {
-        await mongoose.connect('mongodb://localhost/my_database', {
+        await mongoose.connect(mongodb_connection_string, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
